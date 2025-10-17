babf66dd79651f5097dbe95fd0cabbc9181193ec
Lori Garzio <lgarzio@marine.rutgers.edu>
2025-10-17 17:46:01 -0400

    wrapper script

---
+++ run_glider_qc.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+"""
+Author: lgarzio on 12/7/2021
+Last modified: lgarzio 10/17/2025
+This is a wrapper script that imports tools to quality control RUCOOL's glider data.
+"""
+
+import argparse
+import sys
+import scripts
+
+arg_parser = argparse.ArgumentParser(description="QC RUCOOL's glider data",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+arg_parser.add_argument('deployments',
+                        nargs='+',
+                        help='Glider deployment name(s) formatted as glider-YYYYmmddTHHMM')
+
+arg_parser.add_argument('-m', '--mode',
+                        help='Deployment dataset status',
+                        choices=['rt', 'delayed'],
+                        default='rt')
+
+arg_parser.add_argument('-l', '--loglevel',
+                        help='Verbosity level',
+                        type=str,
+                        choices=['debug', 'info', 'warning', 'error', 'critical'],
+                        default='info')
+
+arg_parser.add_argument('-test', '--test',
+                        help='Point to the environment variable key GLIDER_DATA_HOME_TEST for testing.',
+                        action='store_true')
+
+parsed_args = arg_parser.parse_args()
+
+# remove TWRC fill values of 0.00 for CTD and DO
+scripts.remove_zeros.main(parsed_args)
+
+# apply QARTOD QC
+scripts.glider_qartod_qc.main(parsed_args)
+
+# check for severely-lagged CTD profile pairs
+scripts.ctd_hysteresis_test.main(parsed_args)
+
+# summarize QARTOD flags
+scripts.summarize_qartod_flags.main(parsed_args)
+
+# calculate optimal time shift for each segment for variables defined in config files (e.g. DO and pH voltages)
+# requires a deployment time_shift.yml config file in ./glider-deployment/config/qc to run
+scripts.time_shift.main(parsed_args)
+
+# calculate additional science variables (pH, TA, omega and dissolved oxygen in mg/L)
+scripts.add_derived_variables.main(parsed_args)
+
+# move the files to the parent directory to be sent to ERDDAP
+scripts.move_nc_files.main(parsed_args)
+
+sys.exit()
---
+++ scripts/__init__.py
@@ -0,0 +1,7 @@
+from . import add_derived_variables
+from . import ctd_hysteresis_test
+from . import glider_qartod_qc
+from . import move_nc_files
+from . import remove_zeros
+from . import summarize_qartod_flags
+from . import time_shift
